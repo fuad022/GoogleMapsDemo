92273d7caaa0d07d4a958d04a138876647e328e0
f.r.malikov <fuad.malikov@rabitabank.com>
2022-01-21 17:11:19 +0400

    Markers.

--- app/src/main/java/com/example/googlemapsdemo/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapsdemo/MapsActivity.kt
@@ -1,23 +1,27 @@
 package com.example.googlemapsdemo
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.lifecycle.lifecycleScope
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.googlemapsdemo.databinding.ActivityMapsBinding
 import com.example.googlemapsdemo.misc.CameraAndViewport
 import com.example.googlemapsdemo.misc.TypeAndStyle
-import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -55,8 +59,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map = googleMap
 
         val losAngeles = LatLng(34.04692123923977, -118.2474842145839)
+        val losAngeles2 = LatLng(34.125037184403574, -118.38286807004069)
         val newYork = LatLng(40.75525227312982, -74.01902321542899)
-        map.addMarker(MarkerOptions().position(losAngeles).title("Marker in Los Angeles"))
+        val losAngelesMarker =
+            map.addMarker(MarkerOptions()
+                .position(losAngeles)
+                .title("Marker in Los Angeles"))
+        val losAngelesMarker2 =
+            map.addMarker(MarkerOptions()
+                .position(losAngeles2)
+                .title("Marker in Los Angeles")
+                .zIndex(1f))
+
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(losAngeles, 10f))
         map.uiSettings.apply {
             isZoomControlsEnabled = true
@@ -65,34 +79,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 //        lifecycleScope.launch {
 //            delay(4000L)
-//
-////            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraAndViewport.losAngeles), 2000, object : GoogleMap.CancelableCallback {
-////                override fun onFinish() {
-////                    Toast.makeText(this@MapsActivity, "Finished", Toast.LENGTH_SHORT).show()
-////                }
-////
-////                override fun onCancel() {
-////                    Toast.makeText(this@MapsActivity, "Canceled", Toast.LENGTH_SHORT).show()
-////                }
-////            })
-//////            map.animateCamera(CameraUpdateFactory.newLatLngBounds(cameraAndViewport.melbourneBounds, 100), 2000, null)
-//////            map.setLatLngBoundsForCameraTarget(cameraAndViewport.melbourneBounds)
 //        }
-
-        onMapClicked()
-        onMapLongClicked()
     }
 
-    private fun onMapClicked() {
-        map.setOnMapClickListener {
-            Toast.makeText(this, "Single Click", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun onMapLongClicked() {
-        map.setOnMapLongClickListener {
-            map.addMarker(MarkerOptions().position(it).title("New Marker"))
-        }
-    }
 
 }
